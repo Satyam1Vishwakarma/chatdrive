ad8688a4486c57f74de15d83ce920a6d7180f62b
Satyam1Vishwakarma <97113954+Satyam1Vishwakarma@users.noreply.github.com>
2024-10-27 17:09:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ import { Server } from "socket.io";
 import cors from "cors";
 import * as edgedb from "edgedb";
 import { object } from "zod";
-import sqlite3 from "sqlite3";
-import { open } from "sqlite";
 
 const app = express();
 const server = createServer(app);
@@ -28,19 +26,6 @@ app.use(
 
 
 const client = edgedb.createClient();
-
-const db = await open({
-  filename: "chat.db",
-  driver: sqlite3.Database,
-});
-
-await db.exec(`
-    CREATE TABLE IF NOT EXISTS messages (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      client_offset TEXT UNIQUE,
-      content TEXT
-    );
-  `);
 
 const event = {
   "signin response": {
@@ -375,42 +360,6 @@ io.on("connection", (socket) => {
     console.log("end /", reason);
     io.sockets.emit("discontinued");
   });
-});
-
-io.on("connection", async (socket) => {
-  socket.on("chat message", async (msg, clientOffset, callback) => {
-    let result;
-    try {
-      result = await db.run(
-        "INSERT INTO messages (content, client_offset) VALUES (?, ?)",
-        msg,
-        clientOffset
-      );
-    } catch (e) {
-      if (e.errno === 19 /* SQLITE_CONSTRAINT */) {
-        callback();
-      } else {
-        // nothing to do, just let the client retry
-      }
-      return;
-    }
-    io.emit("chat message", msg, result.lastID);
-    callback();
-  });
-
-  if (!socket.recovered) {
-    try {
-      await db.each(
-        "SELECT id, content FROM messages WHERE id > ?",
-        [socket.handshake.auth.serverOffset || 0],
-        (_err, row) => {
-          socket.emit("chat message", row.content, row.id);
-        }
-      );
-    } catch (e) {
-      // something went wrong
-    }
-  }
 });
 
 var port = process.env.PORT || 3001;
